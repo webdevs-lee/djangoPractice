88f7b7ad3517e5e0af39b9d785386898c82f3b7d
webdevs-lee <jsweb1008@gmail.com>
2024-04-23 18:22:29 +0900

    initial commit

---
+++ costaurant/foods/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+    path("index/", views.index), 
+    path("menu/<str:food>/", views.food_detail),
+]
