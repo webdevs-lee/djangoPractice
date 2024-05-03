880cc2594e31d519293535bdfe635df3aa92de2e
webdevs-lee <jsweb1008@gmail.com>
2024-05-03 18:16:34 +0900

    Model 공부 완료

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import Http404
+from foods.models import Menu
 
 # Create your views here.
 def index(request):
+    context = dict()
     today = datetime.today().date()
-    context = {"date": today}
+    menus = Menu.objects.all()
+    context["date"] = today
+    context["menus"] = menus
+    
     return render(request, 'foods/index.html', context=context)
 
 def food_detail(request, food):
