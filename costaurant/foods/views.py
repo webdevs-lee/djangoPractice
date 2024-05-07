e12bd75b56927487fe60ce31c74a27eefd9cf024
webdevs-lee <jsweb1008@gmail.com>
2024-05-07 21:18:08 +0900

    CRUD 프로젝트 추가

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -13,13 +13,15 @@ def index(request):
     
     return render(request, 'foods/index.html', context=context)
 
-def food_detail(request, food):
-    context = {}
-    if food == "chicken":
-        context["name"] = "코딩에 빠진 닭"
-        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
-        context["price"] = 10000
-        context["img_path"] = "foods/images/chicken.jpg"
-    else:
-        raise Http404("이런 음식은 없음.")
+def food_detail(request, pk):
+    context = dict()
+    menu = Menu.objects.get(id=pk)
+    context['menu'] = menu
+    # if food == "chicken":
+    #     context["name"] = "코딩에 빠진 닭"
+    #     context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
+    #     context["price"] = 10000
+    #     context["img_path"] = "foods/images/chicken.jpg"
+    # else:
+    #     raise Http404("이런 음식은 없음.")
     return render(request, 'foods/detail.html', context=context)
