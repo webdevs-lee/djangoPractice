737717bb70c334ed3fbdc35b04b04d63ad7d19fa
webdevs-lee <jsweb1008@gmail.com>
2024-05-14 20:54:14 +0900

    Apply Generic View

--- django/costory/posts/views.py
+++ django/costory/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
@@ -7,62 +9,96 @@ from .forms import PostForm
 def index(request):
     return redirect('post-list')
 
-def post_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 6)
-    current_page_number = request.GET.get('page')
+# def post_list(request):
+#     posts = Post.objects.all()
+#     paginator = Paginator(posts, 6)
+#     current_page_number = request.GET.get('page')
 
-    if current_page_number is None:
-        current_page_number = 1
+#     if current_page_number is None:
+#         current_page_number = 1
 
-    page = paginator.page(current_page_number)
-    return render(request, 'posts/post_list.html', {'page': page})
+#     page = paginator.page(current_page_number)
+#     return render(request, 'posts/post_list.html', {'page': page})
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    context = {'post': post}
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-dt_created']
+    paginate_by = 6
+    page_kwarg = 'page'
 
-    return render(request, 'posts/post_detail.html', context=context)
+# def post_detail(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     context = {'post': post}
 
-def post_create(request):
-    if request.method == 'POST':
-        # title = request.POST['title']
-        # content = request.POST['content']
-        # new_post = Post(
-        #     title = title,
-        #     content = content
-        # )
-        # new_post.save()
-        post_form = PostForm(request.POST) # Binding
-        if post_form.is_valid(): # 유효성 검사를 통과
-            new_post = post_form.save()
-            return redirect('post-detail', post_id=new_post.id)
-        
-    else:
-        # GET
-        post_form = PostForm()
+#     return render(request, 'posts/post_detail.html', context=context)
 
-    return render(request, 'posts/post_form.html', {'form': post_form})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
 
-def post_update(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post_form = PostForm(request.POST, instance=post)
-        if post_form.is_valid():
-            post_form.save()
-            return redirect('post-detail', post_id=post.id)
-    else:
-        #GET
-        post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form': post_form})
 
-def post_delete(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post.delete()
-        return redirect('post-list')
-    else:
-        # GET
-        return render(request, 'posts/post_confirm_delete.html', {'post': post})
+# class PostCreateView(View):
+#     def get(self, request):
+#         post_form = PostForm()
+#         return render(request, 'posts/post_form.html', {'form': post_form})
     
+#     def post(self, request):  
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect('post-detail', post_id=new_post.id)
+#         else:
+#             return render(request, 'posts/post_form.html', {'form': post_form})
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id': self.object.id})
+
+
+# def post_update(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     if request.method == 'POST':
+#         post_form = PostForm(request.POST, instance=post)
+#         if post_form.is_valid():
+#             post_form.save()
+#             return redirect('post-detail', post_id=post.id)
+#     else:
+#         #GET
+#         post_form = PostForm(instance=post)
+#     return render(request, 'posts/post_form.html', {'form': post_form})
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    pk_url_kwarg = 'post_id'
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id': self.object.id})
+
+# def post_delete(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     if request.method == 'POST':
+#         post.delete()
+#         return redirect('post-list')
+#     else:
+#         # GET
+#         return render(request, 'posts/post_confirm_delete.html', {'post': post})
     
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+    def get_success_url(self):
+        return reverse('post-list')
--- mindnote/mindnote/diary/views.py
+++ mindnote/mindnote/diary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse
 from .models import Page
 from .forms import PageForm
 
@@ -7,69 +9,107 @@ from .forms import PageForm
 def index(request):
     return render(request, 'diary/index.html')
 
-def page_list(request):
-    object_list = Page.objects.all()
-    paginator = Paginator(object_list, 8)
+# def page_list(request):
+#     object_list = Page.objects.all()
+#     paginator = Paginator(object_list, 8)
 
-    page_number = request.GET.get('page') # 쿼리 스트링의 key 값으로 현재 page 받아오기
+#     page_number = request.GET.get('page') # 쿼리 스트링의 key 값으로 현재 page 받아오기
 
-    if page_number is None:
-        page_number = 1
+#     if page_number is None:
+#         page_number = 1
     
-    page = paginator.page(page_number)
-    return render(request, 'diary/page_list.html', {'page': page})
+#     page = paginator.page(page_number)
+#     return render(request, 'diary/page_list.html', {'page': page})
 
-def page_detail(request, page_id):
-    context=dict()
-    object = get_object_or_404(Page, id=page_id)
-    context["object"] = object
+class PageListView(ListView):
+    model = Page
+    template_name = 'diary/page_list.html'
+    context_object_name = 'object_list'
+    ordering = ['-dt_created']
+    paginate_by = 8
+    page_kwarg = 'page'
 
-    return render(request, 'diary/page_detail.html', context=context)
+# def page_detail(request, page_id):
+#     context=dict()
+#     object = get_object_or_404(Page, id=page_id)
+#     context["object"] = object
+
+#     return render(request, 'diary/page_detail.html', context=context)
+
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'diary/page_detail.html'
+    pk_url_kwarg = 'page_id'
+    context_object_name = 'object'
 
 def info(request):
     return render(request, 'diary/info.html')
 
-def page_create(request):
-    if request.method == 'POST':
-        # new_page = Page(
-        #     title = request.POST['title'],
-        #     content = request.POST['content'],
-        #     feeling = request.POST['feeling'],
-        #     score = request.POST['score']
-        # )
-        # new_page.save()
-        page_form = PageForm(request.POST)
-        if page_form.is_valid():
-            new_page = page_form.save()
-            return redirect('page-detail', page_id=new_page.id)
+# def page_create(request):
+#     if request.method == 'POST':
+#         # new_page = Page(
+#         #     title = request.POST['title'],
+#         #     content = request.POST['content'],
+#         #     feeling = request.POST['feeling'],
+#         #     score = request.POST['score']
+#         # )
+#         # new_page.save()
+#         page_form = PageForm(request.POST)
+#         if page_form.is_valid():
+#             new_page = page_form.save()
+#             return redirect('page-detail', page_id=new_page.id)
     
-    else:
-        # GET
-        page_form = PageForm()
+#     else:
+#         # GET
+#         page_form = PageForm()
 
-    return render(request, 'diary/page_form.html', {'form': page_form})
+#     return render(request, 'diary/page_form.html', {'form': page_form})
 
-def page_update(request, page_id):
-    object = get_object_or_404(Page, id=page_id)
-    if request.method == 'POST':
-        page_form = PageForm(request.POST, instance=object)
-        if page_form.is_valid():
-            page_form.save()
-            return redirect('page-detail', page_id=object.id)
+class PageCreateView(CreateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.object.id})
+
+# def page_update(request, page_id):
+#     object = get_object_or_404(Page, id=page_id)
+#     if request.method == 'POST':
+#         page_form = PageForm(request.POST, instance=object)
+#         if page_form.is_valid():
+#             page_form.save()
+#             return redirect('page-detail', page_id=object.id)
         
-    else:
-        # GET
-        page_form = PageForm(instance=object)
+#     else:
+#         # GET
+#         page_form = PageForm(instance=object)
 
-    return render(request, 'diary/page_form.html', {'form': page_form})
+#     return render(request, 'diary/page_form.html', {'form': page_form})
 
-def page_delete(request, page_id):
-    object = get_object_or_404(Page, id=page_id)
-    if request.method == 'POST':
-        object.delete()
-        return redirect('page-list')
-    else:
-        # GET
-        return render(request, 'diary/page_confirm_delete.html', {'object': object})
+class PageUpdateView(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+    pk_url_kwarg = 'page_id'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.object.id})
+
+# def page_delete(request, page_id):
+#     object = get_object_or_404(Page, id=page_id)
+#     if request.method == 'POST':
+#         object.delete()
+#         return redirect('page-list')
+#     else:
+#         # GET
+#         return render(request, 'diary/page_confirm_delete.html', {'object': object})
     
-    
+class PageDeleteView(DeleteView):
+    model = Page
+    template_name = 'diary/page_confirm_delete.html'
+    context_object_name = 'object'
+    pk_url_kwarg = 'page_id'
+
+    def get_success_url(self):
+        return reverse('page-list')
